5876f18e07ca4a7510d7e1e750e3589aee51b66a
danpatoneil <danpatoneil@gmail.com>
2024-04-21 15:54:14 -0400

    implemented fs to write to db.json

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const path = require('path');
-const db = require('../Note-Taker/db/db.json');
+const db = require('./db/db.json');
+const fs = require('fs');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+// app.use('/api', api)
+
+app.post('/', (req, res)=>{
+    res.json('Post received');
+})
+
+
+app.get('/', (req, res) => {
+    res.json(db);
+})
+
+// app.delete('/api/notes', (req, res) => {
+
+// })
 
 
 app.get('/notes', (req, res) => {
@@ -20,8 +37,14 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 })
 app.post('/api/notes', (req, res)=>{
-    res.json('Post received');
+    console.log(req.body);
+    db.push(req.body);
+    console.log(db);
+    fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
+        err ? console.error(err):res.json('Post received');
+    })
 })
+
 
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`);
