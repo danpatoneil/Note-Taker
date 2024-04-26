a376aacc025d34597e7669ad0e38b86e9e7a9874
danpatoneil <danpatoneil@gmail.com>
2024-04-26 12:17:30 -0400

    replaced faulty public folder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./db/db.json');
 const fs = require('fs');
+const uuid = require('uuid');
 
 const PORT = process.env.PORT || 3000;
 
@@ -33,18 +34,23 @@ app.get('/notes', (req, res) => {
 app.get('/api/notes', (req, res) => {
     res.json(db);
 })
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-})
 app.post('/api/notes', (req, res)=>{
-    console.log(req.body);
-    db.push(req.body);
-    console.log(db);
+    const { title, text } = req.body;
+    const note = {
+        title,
+        text,
+        "id": uuid.v4(),
+    }
+    db.push(note);
+    console.log(db.title);
     fs.writeFile('./db/db.json', JSON.stringify(db), (err) => {
-        err ? console.error(err):res.json('Post received');
+        err ? console.error(err):res.json(`${note.title} added to task list`);
     })
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`);
